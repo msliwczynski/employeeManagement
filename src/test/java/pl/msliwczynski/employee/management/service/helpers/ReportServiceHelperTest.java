6ba0e715dcdeaa956489899cc475af8a8e40258e
Mateusz Śliwczyński <mateusz.sliwczynski@gmail.com>
2018-05-24 01:02:47 +0200

    changed unit test for ReportServiceHelper

--- src/test/java/pl/msliwczynski/employee/management/service/helpers/ReportServiceHelperTest.java
+++ src/test/java/pl/msliwczynski/employee/management/service/helpers/ReportServiceHelperTest.java
@@ -1,43 +1,60 @@
 package pl.msliwczynski.employee.management.service.helpers;
 
 import org.junit.Test;
+import pl.msliwczynski.employee.management.model.ContactDetails;
+import pl.msliwczynski.employee.management.model.Employee;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ReportServiceHelperTest {
     private ReportServiceHelper reportServiceHelper = new ReportServiceHelper();
+    private Iterable<Employee> employees = getMockEmployees();
 
     @Test
-    public void shouldReturnTrueIfWorkStartDateIsBeforeEndOfYear() {
-        LocalDate workStartDate = LocalDate.of(2017, 12,30);
-        assertTrue(reportServiceHelper.isWorkStartDateBeforeEndOfTheYear(workStartDate, "2017"));
+    public void shouldReturnAvarageSalaryForDecember2017() {
+        List<Double> actual = reportServiceHelper.getSalariesForMonth("2017", Month.of(12), employees);
+        assertEquals(Double.valueOf(0.0), actual.get(0));
+        assertEquals(Double.valueOf(0.0), actual.get(1));
     }
 
     @Test
-    public void shouldReturnFalseIfWorkStartDateIsAfterEndOfYear() {
-        LocalDate workStartDate = LocalDate.of(2018, 1,1);
-        assertFalse(reportServiceHelper.isWorkStartDateBeforeEndOfTheYear(workStartDate, "2017"));
+    public void shouldReturnAvarageSalaryForFebruary() {
+        List<Double> actual = reportServiceHelper.getSalariesForMonth("2018", Month.of(2), employees);
+        assertEquals(Double.valueOf(3000.0), actual.get(0));
+        assertEquals(Double.valueOf(3000.0), actual.get(1));
     }
 
     @Test
-    public void shouldReturnTrueIfWorkEndDateIsNull() {
-        assertTrue(reportServiceHelper.isWorkEndDateNullEqualsOrAfterEndOfYear(null, "2017"));
+    public void shouldReturnAvarageSalaryForMay() {
+        List<Double> actual = reportServiceHelper.getSalariesForMonth("2018", Month.of(5), employees);
+        assertEquals(Double.valueOf(8000.0), actual.get(0));
+        assertEquals(Double.valueOf(4000.0), actual.get(1));
     }
 
     @Test
-    public void shouldReturnTrueIfWorkEndDateIsEqualsEndOfYear() {
-        LocalDate workEndDate = LocalDate.of(2017, 12,31);
-        assertTrue(reportServiceHelper.isWorkEndDateNullEqualsOrAfterEndOfYear(workEndDate, "2017"));
+    public void shouldReturnAvarageSalaryForJuly() {
+        List<Double> actual = reportServiceHelper.getSalariesForMonth("2018", Month.of(7), employees);
+        assertEquals(Double.valueOf(3000.0), actual.get(0));
+        assertEquals(Double.valueOf(3000.0), actual.get(1));
     }
 
-    @Test
-    public void shouldReturnTrueIfWorkEndDateIsAfterEndOfYear() {
-        LocalDate workEndDate = LocalDate.of(2018, 1,1);
-        assertTrue(reportServiceHelper.isWorkEndDateNullEqualsOrAfterEndOfYear(workEndDate, "2017"));
+    private Iterable<Employee> getMockEmployees() {
+        List<Employee> employeeList = new ArrayList<>();
+        ContactDetails johnContactDetails = new ContactDetails("7123456789","5th Avenue, 123", "New York");
+        Employee john = new Employee("John", "Smith", LocalDate.of(2018, 1, 1), LocalDate.of(9999,12,31), 3000.00, johnContactDetails);
+
+        ContactDetails adamContactDetails = new ContactDetails("123456789","Bracka 2", "Warszawa");
+        Employee adam = new Employee("Adam", "Kowalski", LocalDate.of(2018, 4, 1), LocalDate.of(2018,7,31), 5000.00, adamContactDetails);
+
+        employeeList.add(john);
+        employeeList.add(adam);
+
+        return employeeList;
     }
 
 }
